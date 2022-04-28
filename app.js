2ea7d2652df9fa62c047c058705a5decc4928d9e
monarch <seeker99157@gmail.com>
2022-04-27 17:36:31 -0800

    added counter functionality

--- app.js
+++ app.js
@@ -1,5 +1,63 @@
+// Navbar
 const primaryNav = document.querySelector('.primary-nav');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+
+// counter
+const counters = document.querySelectorAll('.numbers');
+const speed = 400;
+
+// show navbar on scroll up
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll <= 0) {
+        body.classList.remove("scroll-up")
+    }
+
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-up");
+        body.classList.add("scroll-down");
+    }
+
+    if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down");
+        body.classList.add("scroll-up");
+    }
+
+
+    lastScroll = currentScroll;
+})
+
+// counter loop
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const inc = target / speed;
+
+        if(count < target) {
+            counter.innerText = count + inc;
+            setTimeout(updateCount, 1)
+        } else{
+            counter.innerText = target;
+        }
+
+    }
+
+    // runs counter functionality after 520px
+    window.addEventListener("scroll", ()=> {
+    if(window.pageYOffset > 520) {
+       updateCount();
+    }
+})
+});
+
+// navbar opening and closing functionality
 
 navToggle.addEventListener('click', ()=> {
     const visibility = primaryNav.getAttribute('data-visible');
@@ -11,4 +69,4 @@ navToggle.addEventListener('click', ()=> {
         primaryNav.setAttribute('data-visible', false)
         navToggle.setAttribute('aria-expanded', false)
     }
-}) 
+})
