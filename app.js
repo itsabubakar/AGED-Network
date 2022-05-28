b83db704aad5b3850981bcd62e7e9777d0350b2f
monarch <seeker99157@gmail.com>
2022-05-28 20:48:13 +0100

    removing placeholder and adding real info

--- app.js
+++ app.js
@@ -78,16 +78,33 @@ counters.forEach(counter => {
 })
 });
 
-// navbar opening and closing functionality
+// carousel slide
 
-// navToggle.addEventListener('click', ()=> {
-//     const visibility = primaryNav.getAttribute('data-visible');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-//     if(visibility === "false") {
-//         primaryNav.setAttribute('data-visible', true)
-//         navToggle.setAttribute('aria-expanded', true)
-//     } else if (visibility === "true") {
-//         primaryNav.setAttribute('data-visible', false)
-//         navToggle.setAttribute('aria-expanded', false)
-//     }
-// })
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+  slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+} 
